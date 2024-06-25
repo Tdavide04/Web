01bd68e13b96f34c0df1e3c4ca540504ee617e55
Tdavide04 <tdavide833@gmail.com>
2024-06-25 17:31:43 +0200

    update

--- Lezioni/sitoweb/server.js
+++ Lezioni/sitoweb/server.js
@@ -8,10 +8,16 @@ var iPortaTcp = 4201;
 var sIpAddress = "127.0.0.1"
 
 app.listen(iPortaTcp,sIpAddress, () => console.log('API is running on http://' + sIpAddress + ':' + iPortaTcp));
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+//const bodyParser = require('body-parser');
+//app.use(bodyParser.urlencoded({ extended: true }));
 //PRIMA ROUTE
 app.get('', (req, res) => {
-//console.log("Mi hai chiesto la pagina iniziale");
-//res.sendFile("index.html", { root: './htdocs' });
+console.log("Mi hai chiesto la pagina iniziale");
+res.sendFile("index.html", { root: './htdocs' });
+});
+
+//SECONDA ROUTE
+app.get('/registrati.html', (req, res) => {
+    console.log("Mi hai chiesto la pagina di registrazione");
+    res.sendFile("registra.html", { root: './htdocs'});
 });
