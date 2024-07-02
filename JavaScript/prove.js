225f546d088aaa282667f9d3e50e413d8d7aac44
Tdavide04 <tdavide833@gmail.com>
2024-07-02 13:55:03 +0200

    update

--- JavaScript/prove.js
+++ JavaScript/prove.js
@@ -1,6 +1,57 @@
 //console.log("ciao")
-const prompt = require("prompt-sync")()
+/*function capitalizeFirstLetter(string)
+{
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
 
-const str = prompt("Enter a string: ")
-console.log("Hai inserito " + str)
-process.exit(0)
+const prompt = require("prompt-sync")()
+var str = prompt("Enter a string: ")
+console.log("Hai inserito prima " + str)
+str = capitalizeFirstLetter(str)
+console.log("Hai inserito dopo " + str)
+*/
+
+function check(sNum)
+{
+    for ( i = 0; i < sNum.length; i++) 
+        
+    {
+    if ((sNum.charAt(i) < '0') || (sNum.charAt(i)>'9'))
+        return false
+    }
+    return true
+}
+
+function parseIntMy(sNum)
+{
+    bEsito = check(sNum)
+    if(bEsito==true)
+        return parseInt(sNum)
+    var sNum1 = "0"
+    for(i=0; i<3; i++)
+        {
+        sNum1 = prompt("Attenzione, numero errato, riprova")
+        bEsito = check(sNum1)
+        if(bEsito==true)
+            return parseInt(sNum1)
+        }
+
+    return null 
+}
+
+
+const prompt = require("prompt-sync")()
+var sPrimoNum = prompt("Inserisci il primo numero ")
+var bEsito = parseIntMy(sPrimoNum)
+if(bEsito == true)
+    console.log("Mi hai passato un numero")
+else 
+    console.log("Non mi hai passato un numero")
+
+var sSecondoNum = prompt("Inserisci il secondo numero ")
+check(sSecondoNum)
+var iSecondNum = parseInt(sSecondoNum)
+
+var sSomma = iPrimoNum + iSecondNum
+
+console.log("La somma vale " + sSomma)
